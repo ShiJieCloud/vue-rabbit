e5e2982011bae1489df8c2b59772613dcf7e4c5a
ShiJieCloud <1500492856@qq.com>
2023-06-20 21:33:21 +0800

    Layout 页面

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-   <div>我是首页
-    <!--二级路由出口组件-->
-    <RouterView/>
-   </div> 
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
