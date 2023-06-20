37a31fc85eb1a639dba9d113c0f29daf652f3cfc
ShiJieCloud <1500492856@qq.com>
2023-06-20 20:20:40 +0800

    封装 axios

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//测试接口函数
+import { getCategoryAPI } from './apis/testAPI'
+getCategoryAPI().then(res=>{
+    console.log(res);
+})
 const app = createApp(App)
 
 app.use(createPinia())
