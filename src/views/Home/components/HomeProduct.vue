4f78a1595cf3cabd6cd49f33198dfc68485d7678
ShiJieCloud <1500492856@qq.com>
2023-06-21 16:07:39 +0800

    商品列表

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -1,15 +1,16 @@
 <script setup>
 import HomePanel from './HomePanel.vue'
+import GoodsItem from '@/views/Home/components/GoodsItem.vue'
 import { getGoodsAPI } from '@/apis/home';
-import { ref,onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const goodsProduct = ref([])
-const getGoodList = async()=>{
+const getGoodList = async () => {
     const res = await getGoodsAPI()
     goodsProduct.value = res.result
 }
 
-onMounted(()=>{
+onMounted(() => {
     getGoodList()
 })
 
@@ -17,27 +18,22 @@ onMounted(()=>{
 
 <template>
     <div class="home-product">
-       <HomePanel :title="cate.name" v-for="cate in goodsProduct" :key="cate.id">
-      <div class="box">
-        <RouterLink class="cover" to="/">
-          <img v-img-lazy="cate.picture" />
-          <strong class="label">
-            <span>{{ cate.name }}馆</span>
-            <span>{{ cate.saleInfo }}</span>
-          </strong>
-        </RouterLink>
-        <ul class="goods-list">
-          <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
-          </li>
-        </ul>
-      </div>
-    </HomePanel> 
+        <HomePanel :title="cate.name" v-for="cate in goodsProduct" :key="cate.id">
+            <div class="box">
+                <RouterLink class="cover" to="/">
+                    <img v-img-lazy="cate.picture" />
+                    <strong class="label">
+                        <span>{{ cate.name }}馆</span>
+                        <span>{{ cate.saleInfo }}</span>
+                    </strong>
+                </RouterLink>
+                <ul class="goods-list">
+                    <li v-for="good in cate.goods" :key="good.id">
+                        <GoodsItem :good="good" />
+                    </li>
+                </ul>
+            </div>
+        </HomePanel>
     </div>
 </template>
 
@@ -126,42 +122,6 @@ onMounted(()=>{
                 &:nth-child(4n) {
                     margin-right: 0;
                 }
-            }
-        }
-
-        .goods-item {
-            display: block;
-            width: 220px;
-            padding: 20px 30px;
-            text-align: center;
-            transition: all .5s;
-
-            &:hover {
-                transform: translate3d(0, -3px, 0);
-                box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-            }
-
-            img {
-                width: 160px;
-                height: 160px;
-            }
-
-            p {
-                padding-top: 10px;
-            }
-
-            .name {
-                font-size: 16px;
-            }
-
-            .desc {
-                color: #999;
-                height: 29px;
-            }
-
-            .price {
-                color: $priceColor;
-                font-size: 20px;
             }
         }
     }
