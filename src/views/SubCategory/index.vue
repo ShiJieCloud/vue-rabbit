a6ecf9c33496e722b0d657367d8fb96c584119bf
ShiJieCloud <1500492856@qq.com>
2023-06-22 23:23:02 +0800

    二级商品列表信息

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { getCategoryFilterAPI } from '@/apis/category';
+import { getCategoryFilterAPI, getSubCategoryAPI } from '@/apis/category'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import GoodsItem from '@/views/Home/components/GoodsItem.vue'
 
 const route = useRoute()
 const categoryData = ref({})
@@ -12,6 +13,23 @@ const getCategoryData = async () => {
 
 onMounted(() => {
     getCategoryData()
+})
+
+// 获取基础列表数据
+const goodList = ref([])
+const reqData = ref({
+    categoryId: route.params.id,
+    page: 1,
+    pageSize: 20,
+    sortField: 'publishTime'
+})
+
+const getGoodList = async() => {
+    const res = await getSubCategoryAPI(reqData.value)
+    goodList.value = res.result.items
+}
+onMounted(()=>{
+    getGoodList()
 })
 
 </script>
@@ -35,6 +53,7 @@ onMounted(() => {
             </el-tabs>
             <div class="body">
                 <!-- 商品列表-->
+                <GoodsItem v-for="good in goodList" :good="good" :key="good.id" />
             </div>
         </div>
     </div>
