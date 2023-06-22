d2dd7600faea8d5d88d27472d3f92951053327d2
ShiJieCloud <1500492856@qq.com>
2023-06-22 22:16:35 +0800

    二级分类路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,10 @@ const router = createRouter({
         {
           path: 'category/:id',
           component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
         }
       ]
     },
