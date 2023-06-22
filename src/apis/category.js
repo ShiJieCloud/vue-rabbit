6a4bd17e508931a36e56125fb1325843930353e9
ShiJieCloud <1500492856@qq.com>
2023-06-22 22:48:07 +0800

    二级分类页面包屑

--- src/apis/category.js
+++ src/apis/category.js
@@ -13,3 +13,17 @@ export const getCategoryAPI = (id) => {
         }
     })
 }
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id 
+ * @return {*}
+ */
+export const getCategoryFilterAPI = (id) => {
+    return http({
+        url: '/category/sub/filter',
+        params: {
+            id
+        }
+    })
+}
--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,4 +1,18 @@
 <script setup>
+import { getCategoryFilterAPI } from '@/apis/category';
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const categoryData = ref({})
+const getCategoryData = async () => {
+    const res = await getCategoryFilterAPI(route.params.id)
+    categoryData.value = res.result
+}
+
+onMounted(() => {
+    getCategoryData()
+})
 
 </script>
 
@@ -8,9 +22,9 @@
         <div class="bread-container">
             <el-breadcrumb separator=">">
                 <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                <el-breadcrumb-item :to="{ path: '/' }">居家
+                <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName }}
                 </el-breadcrumb-item>
-                <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+                <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
         <div class="sub-container">
